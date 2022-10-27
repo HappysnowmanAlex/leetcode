58e500b6da72eb8bfa65d609ca95ff8c7f5cf8ac
zivnadel <zywn1414@gmail.com>
2022-10-28 00:17:33 +0300

    .

--- cpp-solutions/VowelsOfAllSubstrings.cpp
+++ cpp-solutions/VowelsOfAllSubstrings.cpp
@@ -1,20 +1,17 @@
 #include <string>
 
-class Solution
-{
+class Solution {
 public:
-    long long countVowels(std::string word)
-    {
-        long long res = 0;
+  long long countVowels(std::string word) {
+    long long res = 0;
 
-        
-        size_t n = word.size();
-        const std::string vowels = "aeiou";
+    size_t n = word.size();
+    const std::string vowels = "aeiou";
 
-        for (size_t i = 0; i < n; i++)
-            if (vowels.find(word[i]) != std::string::npos)
-                res += (i + 1) * (n - i);
+    for (size_t i = 0; i < n; i++)
+      if (vowels.find(word[i]) != std::string::npos)
+        res += (i + 1) * (n - i);
 
-        return res;
-    }
+    return res;
+  }
 };
